cb1b44d9995d6e310bc9aabb4baf8dcaebc21e54
omesina <ormesina@outlook.com>
2020-11-05 15:06:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //Importar librerias
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = "static-v3";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE  = "inmutable-v1";
 
 const APP_SHELL = [
